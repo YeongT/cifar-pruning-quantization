0ee478af01577cbaec00bcd9ccdafb1e429c0076
YEONGCHANG YUN <94744740+YeongT@users.noreply.github.com>
2026-02-05 10:56:46 +0900

    change epoch settings

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ print('==> Building model..')
 # net = DenseNet121()
 # net = ResNeXt29_2x64d()
 # net = MobileNet()
-# net = MobileNetV2()
+net = MobileNetV2()
 # net = DPN92()
 # net = ShuffleNetG2()
 # net = SENet18()
@@ -148,7 +148,7 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+200):
+for epoch in range(start_epoch, start_epoch+10):
     train(epoch)
     test(epoch)
     scheduler.step()
